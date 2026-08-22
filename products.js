7df6f98f4cf6410aed5cf49bfe1471a3ceb425a5
produtoshitech <produtoshitech@gmail.com>
2026-08-22 19:18:37 -0300

    Update products.js

--- products.js
+++ products.js
@@ -121,7 +121,7 @@ const products = [
   
  {
     title: "Oferta especial — Bikelete Bicicleta Elétrica 60km/h 500w",
-    image: "assets/Screenshot_20260822-171145_Mercado Livre.png",
+    image: "assets/Screenshot_20260822-191411_Mercado Livre.png",
     link: "https://meli.la/2DujWCG"
   },
   
